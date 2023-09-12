f1522c7a1e788687c8cd856b10e108e5d3dcab2d
minde8888 <mindebaltru@gmail.com>
2023-09-12 22:49:03 +0200

    Add project files.

---
+++ WebScrapingSelenium/Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://www.google.no/");
+
+        // Click on the "I agree" button if it's present
+        try
+        {
+            var agreeButton = driver.FindElement(By.XPath("//*[@id=\"L2AGLb\"]/div"));
+            if (agreeButton.Displayed && agreeButton.Enabled)
+            {
+                agreeButton.Click();
+            }
+        }
+        catch (NoSuchElementException)
+        {
+            // Handle the case where the "I agree" button is not present
+        }
+
+        var element = driver.FindElement(By.XPath("//*[@id=\"APjFqb\"]"));
+        element.SendKeys("Sko");
+        element.Submit();
+
+        var titles = driver.FindElements(By.XPath("//*[@id=\"rso\"]/div/div/div/div[1]/div/div/span/a/h3"));
+
+        foreach (var item in titles)
+        {
+            Console.WriteLine(item.Text);
+        }
+
+        // Close the WebDriver
+        driver.Quit();
+    }
+}
